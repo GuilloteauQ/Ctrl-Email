b79d78c06cda8f97a7cd36684a7c35718b99dc2d
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-04-11 18:45:11 +0200

    init

---
+++ shell.nix
@@ -0,0 +1,11 @@
+with (import <nixpkgs>{});
+
+stdenv.mkDerivation {
+  name = "shell";
+  buildInputs = [
+    python37Full
+    python37Packages.google_api_python_client
+    python37Packages.google-auth-httplib2
+    python37Packages.google-auth-oauthlib
+  ];
+}
