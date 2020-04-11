36a43d42e012322451386175782189856888061e
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-04-12 00:09:55 +0200

    update nix shell

--- shell.nix
+++ shell.nix
@@ -7,5 +7,6 @@ stdenv.mkDerivation {
     python37Packages.google_api_python_client
     python37Packages.google-auth-httplib2
     python37Packages.google-auth-oauthlib
+    python37Packages.notify2
   ];
 }
