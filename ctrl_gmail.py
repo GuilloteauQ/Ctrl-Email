24ed37e0ec0539bb5947dd0e32ebb78652b60a0e
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-04-12 00:08:45 +0200

    add desktop notifications

--- ctrl_gmail.py
+++ ctrl_gmail.py
@@ -4,6 +4,8 @@ import os.path
 import time
 import json
 import sys
+import notify2
+from datetime import datetime
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -137,15 +139,27 @@ def main():
         config_file_name = "config.json"
 
     (controller, client) = extract_config(config_file_name)
+    notify2.init("Ctrl-Gmail")
+    notifyer = notify2.Notification(None, icon="")
+    notifyer.set_urgency(notify2.URGENCY_NORMAL)
+    notifyer.set_timeout(10000)
+
+    sleep_time = controller.get()
     while True:
-        sleep_time = controller.get()
-        print(" [*] Sleeping for {} seconds".format(sleep_time))
+        # print(" [*] Sleeping for {} seconds".format(sleep_time))
         time.sleep(int(sleep_time))
-        print(" [*] Reading emails")
+        # print(" [*] Reading emails")
         (nb_new_emails, nb_unread_emails, error) = client.plant()
-        print(" [*] You have {} new message(s) and {} unread message(s)".format(nb_new_emails, nb_unread_emails))
-        print(" [*] Updating the controller (error: {})".format(error))
+        # print(" [*] You have {} new message(s) and {} unread message(s)".format(nb_new_emails, nb_unread_emails))
+        # print(" [*] Updating the controller (error: {})".format(error))
         controller.update(error)
+        sleep_time = controller.get()
+        if nb_new_emails + nb_unread_emails > 0:
+            title = "Ctrl-Gmail: {} new and {} unread message(s)".format(nb_new_emails, nb_unread_emails )
+            next_check = datetime.fromtimestamp(int(time.time() + sleep_time)).strftime("%H:%M:%S")
+            description = "Next check at {}".format(next_check)
+            notifyer.update(title, description) 
+            notifyer.show()
 
 if __name__ == '__main__':
     main()
