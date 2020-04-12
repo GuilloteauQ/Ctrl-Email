e4752e4018fa559f6cfd1b9dee4aaa95a9637144
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-04-12 15:34:05 +0200

    refactor

--- default.nix
+++ default.nix
@@ -1,20 +1,37 @@
 with (import <nixpkgs>{});
 
-python37Packages.buildPythonPackage rec {
-  name = "ctrl-email-${version}";
-  version = "1.0";
+let
+  app_name = "ctrl-email";
+  app_version = "1.0";
+in {
+  ctrl_email = python37Packages.buildPythonPackage rec {
+    name = "${app_name}-${version}";
+    version = "${app_version}";
 
-  src = fetchTarball("https://github.com/GuilloteauQ/Ctrl-Email/tarball/master");
-  propagatedBuildInputs = with python37Packages; [
-    google_api_python_client
-    google-auth-httplib2
-    google-auth-oauthlib
-    notify2
-  ];
+    src = fetchTarball("https://github.com/GuilloteauQ/Ctrl-Email/tarball/master");
+    propagatedBuildInputs = with python37Packages; [
+      google_api_python_client
+      google-auth-httplib2
+      google-auth-oauthlib
+      notify2
+    ];
 
-  doCheck = false;
+    doCheck = false;
 
-  postInstall = ''
-    cp -r app/ $out
-  '';
+    postInstall = ''
+      cp -r app/ $out
+    '';
+  };
+
+  # systemd.user.services.ctrl-email = {
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.ctrl_email}/ctrl_email";
+  #     KillMode = "process";
+  #     Restart = "always";
+  #     Type = "simple";
+  #   };
+  # };
+
+  # systemd.services.ctrl-email.enable = true;
 }
